48c2636dc45174f6f30387432e6856d3919d5a21
kdgosik <kdgosik@gmail.com>
2018-10-10 14:49:25 -0400

    working version of python package

--- python/pypi_package/merkledag/merkledag/merkledag.py
+++ python/pypi_package/merkledag/merkledag/merkledag.py
@@ -4,8 +4,20 @@ import json
 import datetime as date
 
 
+## chunks a file and hashes the content
+def hash_file(name):
+    BLOCKSIZE = 65536
+    sha = hashlib.sha256()
+    with open(name, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            sha.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return(sha.hexdigest())
+
+
+
 ## Block class for creating block structure
-## TODO include previous_hash when hashing the file?
 ## may need to make previous_hash a list of previous hashes
 class Block:
     def __init__(self, name, timestamp, previous_hashes):
@@ -15,85 +27,82 @@ class Block:
         self.previous_hashes = sorted(previous_hashes)
         self.hash = self.hash_block()
 
+        ## defined above
     def hash_file(self):
-        BLOCKSIZE = 65536
-        hasher = hashlib.sha256()
-        with open(self.name, 'rb') as afile:
-            buf = afile.read(BLOCKSIZE)
-            while len(buf) > 0:
-                hasher.update(buf)
-                buf = afile.read(BLOCKSIZE)
-        return(hasher.hexdigest())
+        return hash_file(self.name)
 
         ## TODO not sure if I need to include the timestamp
     def hash_block(self):
         sha = hashlib.sha256()
-        sha.update(str(self.name) +
-                   # str(self.timestamp) +
+        sha.update((str(self.name) +
                    str(self.data) +
-                   str(self.previous_hashes))
+                   str(self.previous_hashes)).encode('utf-8'))
         return sha.hexdigest()
 
 
+## Add Block to replace create_genesis_block and next_block
+def add_block(file_name, depend_files):
+    this_name = file_name # tracks current name of file
+    this_timestamp = date.datetime.now() # creates timestamp
+
+    if depend_files == "": # checks if a genesis file (no previous file dependencies)
+            # if merkledag file doesn't exist, create it
+        if not os.path.isfile('merkledag_file.json'):
+            d = []
+            ## writing with indetation for a pretty print
+            with open('merkledag_file.json', 'w') as out_file:
+                json.dump(d, out_file, indent=4)
+
+        ## creates a new block
+        newBlock = Block(this_name, this_timestamp, "")
+
+        ## add new block to merkledag file
+        with open('merkledag_file.json', 'r') as in_file:
+            d = json.load(in_file)
+            d.append(newBlock.__dict__)
+
+        ## writing with indetation for a pretty print
+        with open('merkledag_file.json', 'w') as out_file:
+            json.dump(d, out_file, indent=4)
+
+    else:  ## if depend are given
+        previous_hashes = []
+        ## loop through each dependency
+        for l in depend_files:
+            file_data_hash = hash_file(l)
+            file_block = lookup_file(file_data_hash)
+            file_block_hash = file_block['hash']
+            previous_hashes.append(file_block_hash)
+
+        newBlock = Block(this_name, this_timestamp, previous_hashes)
+
+        ## add new block to merkledag file
+        with open('merkledag_file.json', 'r') as in_file:
+            d = json.load(in_file)
+            d.append(newBlock.__dict__)
+
+        ## writing with indetation for a pretty print
+        with open('merkledag_file.json', 'w') as out_file:
+            json.dump(d, out_file, indent=4)
 
 
 
-## genesis block creation function
-## TODO how to create genesis block for each script and raw data file
-## TODO differentiate between script and datasets at the hash level?
-def create_genesis_block(file_name):
-    # Manually construct a block by giving a file name
-    # TODO add error checking
-    newBlock = Block(file_name, date.datetime.now(), ['GenesisFile'])
-
-    ## add new block to merkledag file
+## searches merkledag_file.json for the content hash of the file and returns it
+def lookup_file(file_data_hash):
     with open('merkledag_file.json', 'r') as in_file:
         d = json.load(in_file)
-        d.append(newBlock.__dict__)
-
-    ## writing with indetation for a pretty print
-    with open("merkledag_file.json", "w") as out_file:
-        json.dump(d, out_file, indent=4, sort_keys=True)
-
-    return newBlock
+    for i in d:
+        if i['data'] in file_data_hash:
+            return(i) ## file content hash
 
 
 
-## link a new file to an existing block
-## TODO input a list of last_blocks to link together?
-## Would the file name in this case be the output of running the script?
-def next_block(file_name, last_block):
-    # TODO extend to include multiple hashes for each file it depends on
-    this_name = file_name
-    this_timestamp = date.datetime.now()
-    this_hash = last_block.hash
-    newBlock = Block(this_name, this_timestamp, this_hash)
 
-    ## add new block to merkledag file
-    with open('merkledag_file.json', 'r') as in_file:
-        d = json.load(in_file)
-        d.append(newBlock.__dict__)
-
-    ## writing with indetation for a pretty print
-    with open("merkledag_file.json", "w") as out_file:
-        json.dump(d, out_file, indent=4, sort_keys=True)
-
-    return newBlock
 
 
 
 
 ## TODO other possible functions needed
-
-## initialization function to create merkledag file
-def init():
-    """
-    initialize the repository to create csv and/or json file to hold the dag file of blocks.
-    """
-    out = [{'name': 'root', 'timestamp': str(date.datetime.now()), 'hash': 'GenesisFile', 'previous_hash': ''}]
-    outjson = json.dumps(out)
-    with open("merkledag_file.json", "w") as f:
-        f.write(outjson)
 
 
 
@@ -103,13 +112,6 @@ def jupyter_cell_to_script():
     script to make a jupyter notebook cell into an independent python script using template.py
     """
 
-
-
-
-def lookup():
-    """
-    Look up function to see if hash and/or file already exists in merkle dag
-    """
 
 
 

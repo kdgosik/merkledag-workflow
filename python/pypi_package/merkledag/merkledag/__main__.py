7de39d8b2b3a70f538f641d9fa068c76efdc5837
kdgosik <kdgosik@gmail.com>
2018-10-11 19:47:48 -0400

    updated cli to include output file in argparse

--- python/pypi_package/merkledag/merkledag/__main__.py
+++ python/pypi_package/merkledag/merkledag/__main__.py
@@ -7,21 +7,26 @@ import argparse
 # initiate the parser
 parser = argparse.ArgumentParser(description='Hash link two or more files together')
 
-# add long and short argument (args.width)
+# add file input argument (args.input)
 parser.add_argument("--input", "-i", help="Input file to hash")
 
-# add long and short argument (args.width)
+# add list of links argument (args.link)
 parser.add_argument("--link", "-l", nargs = '*', help="List of files to hash link to input file.", default = [])#, action = 'append')
+
+# add output file name argument (args.output)
+parser.add_argument("--output", "-o", nargs = '?', help="Ouput file name (defualts to ./merkledag_file.json).", default = 'merkledag_file.json')#, action = 'append')
 
 # read arguments from the command line
 args = parser.parse_args()
 
 def main():
     data_file = args.input
-    print(data_file)
+    print('Added '+ data_file + ' to the merkle dag!')
     previous_list = args.link
-    print(previous_list)
-    print(merkledag.add_block(data_file, previous_list))
+    print('Linked ' + data_file + ' to the file(s): ' + ', '.join(previous_list))
+    output_file = args.output
+    print('Placed output in ' + output_file)
+    merkledag.add_block(data_file, previous_list, output_file)
 
 
 if __name__ == '__main__':
--- python/pypi_package/merkledag/setup.py
+++ python/pypi_package/merkledag/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='merkledag',
-      version='0.5',
+      version='0.75',
       description='Merkle Dag Workflow Helpers',
       url='http://github.com/kdgosik/MerkleDAGWorkflow',
       author='Kirk Gosik',
